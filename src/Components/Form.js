5d98fc1b6c1e321e532b8f540ad27ad2bb10320e
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-02 18:50:44 +0100

    Form component and styles built

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,8 +3,27 @@ import "../Styles/Form.css"
 
 export default function Form() {
     return (
-        <form>
-                
-        </form>
+        <main>
+            <form>
+                <div className="input-container">
+                <input 
+                    type="text"
+                    className="input" 
+                    placeholder="top text"
+                /> 
+                <input 
+                    type="text"
+                    className="input"  
+                    placeholder="bottom text" 
+                /> 
+                </div>
+                <button 
+                    className="new-meme-button" 
+                    type="submit"
+                >
+                    Get a new meme image
+                </button>
+            </form>
+        </main>
     )
 }
