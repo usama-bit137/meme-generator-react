8087b3c87de628f7087286221de14fa59b6f8f0e
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-03 00:31:44 +0100

    Button click event generates a random meme inage from links contained within memesData.json file. Image tag added in Form component. Styles added to image component.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,29 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import "../Styles/Form.css"
 
 export default function Form() {
+    const [memeImage, setMemeImage] = useState('');
+
+    function randomMeme() {
+        let memesArray = require("../memesData.json").data.memes,
+            randomNumber = Math.floor(Math.random()*memesArray.length)
+            setMemeImage(memesArray[randomNumber].url); 
+    }
+
     return (
         <main>
-            <form>
+            <div>
                 <div className="input-container">
-                <input 
-                    type="text"
-                    className="input" 
-                    placeholder="top text"
-                /> 
-                <input 
-                    type="text"
-                    className="input"  
-                    placeholder="bottom text" 
-                /> 
+                    <input 
+                        type="text"
+                        className="input" 
+                        placeholder="top text"
+                    /> 
+                    <input 
+                        type="text"
+                        className="input"  
+                        placeholder="bottom text" 
+                    /> 
                 </div>
                 <button 
+                    onClick = {randomMeme}
                     className="new-meme-button" 
                     type="submit"
                 >
                     Get a new meme image
                 </button>
-            </form>
+            </div>
+            <img 
+                className="meme-image"
+                src={memeImage} 
+            />
         </main>
     )
 }
