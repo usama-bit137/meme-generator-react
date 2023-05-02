068137735872583fb6c9d68279c65cba6af2f394
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-02 18:06:43 +0100

    Footer Component rendered

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import '../Styles/App.css';
+import React from "react"
+import '../Styles/App.css'
 import Header from "./Header"
+import Form from "./Form"
+import Footer from "./Footer"
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
   );
 }
